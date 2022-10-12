8a333b3ffe2a0964938fc45869a975223447311e
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-10-12 18:16:29 +0100

    UI and HUD

--- src/App.svelte
+++ src/App.svelte
@@ -13,19 +13,12 @@
 <div class="space-ctr" id="space-ctr">
 </div>
 <div bind:this={el} id="nft-ctr" class="nft-ctr">
-	<div class="container" id="nft-img">
-	</div>
-	<div id="nft-overlay-holder" class="svelte-n8t4is">
-
-		<div id="nft-overlay" class="nft-3d-overlay svelte-n8t4is">
-			<span id="pos-display" class="pos">x:0 , y:0 , z:0</span>
-			<!--<select id="layout" class="svelte-n8t4is">
-				<option value="amphitheater">Amphitheater</option><option value="round_showroom">roundshowroom</option><option value="art_showroom">art_showroom</option><option value="aztec_temple">aztec_temple</option><option value="big_room">big_room</option></select> -->
-				
-		</div>
-
-	</div>
 	<div class="d3d-btn view-vr-btn" style="display: none;">Enter VR</div>
+	<div id="view-full" class="d3d-btn view-vr-btn">Full Screen</div>
 
 </div>	
-<div id="hud-content"></div>
+<div id="hud-content">
+<div id="hud-text"></div>
+<div class="buttons">
+<button class="nft-button-grad" id="close-hud">Close</button></div>
+</div>
