e13487b5da34b86876b81023f3e22193318ac9ed
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-10-13 10:33:21 +0100

    UI formatting

--- src/App.svelte
+++ src/App.svelte
@@ -11,14 +11,17 @@
 </script>
 <div class="loader-ctr"></div>
 <div class="space-ctr" id="space-ctr">
+<div id="hud-content">
+<div id="hud-text"></div>
+<div class="buttons">
+<button class="nft-button-grad" id="view-page">View NFT Page</button>
+<button class="nft-button-grad" id="buy-now">Buy Now</button>
+<button class="nft-button-grad" id="close-hud">Close</button></div>
+</div>
+
 </div>
 <div bind:this={el} id="nft-ctr" class="nft-ctr">
 	<div class="d3d-btn view-vr-btn" style="display: none;">Enter VR</div>
 	<div id="view-full" class="d3d-btn view-vr-btn">Full Screen</div>
 
 </div>	
-<div id="hud-content">
-<div id="hud-text"></div>
-<div class="buttons">
-<button class="nft-button-grad" id="close-hud">Close</button></div>
-</div>
