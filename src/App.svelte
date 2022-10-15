5a83af5a01e31236d426fcaa98fdd30b7f4668c7
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-10-15 16:13:12 +0100

    UI update

--- src/App.svelte
+++ src/App.svelte
@@ -10,20 +10,23 @@
 	});
 </script>
 <div class="loader-ctr"></div>
-<div class="space-ctr" id="space-ctr">
-<div id="hud-content">
-<div id="hud-text"></div>
-<div class="buttons">
-<button class="nft-button-grad" id="view-page">View NFT Page</button>
-<button class="nft-button-grad" id="buy-now">Buy Now</button>
-<button class="nft-button-grad" id="close-hud">Close</button></div>
-</div>
+<div bind:this={el} class="space-ctr" id="space-ctr">
+	<div style="display:none;" class="statusbar"></div>
+	<div style="display:none;" id="heart" class="d3d-btn-top heart">❤</div>
+	<div style="display:none;" id="diamond-count" class="d3d-btn-top"><span id="d-count">0</span> X 💎</div>
+	<div style="display:none;" id="confirm" class="d3d-btn-top confirm-transaction"><buton class="nft-button-grad" id="trx-confirm">Confirm Send</div>
 
-</div>
-<div bind:this={el} id="nft-ctr" class="nft-ctr">
+	<div id="hud-content">
+		<div id="hud-text"></div>
+		<div class="buttons">
+		<button class="nft-button-grad" id="view-page">View NFT Page</button>
+		<button class="nft-button-grad" id="buy-now">Buy Now</button>
+		<button class="nft-button-grad" id="close-hud">Close</button></div>
+	</div>
+	<div class="toolbar"></div>
 	<div class="d3d-btn view-vr-btn" style="display: none;">Enter VR</div>
 	<div id="view-full" class="d3d-btn view-vr-btn">Full Screen</div>
-	<div id="give-diamond" class="d3d-btn view-vr-btn">💎</div>
-	<div id="give-heart" class="d3d-btn view-vr-btn">❤</div>
-
-</div>	
+	<div id="give-diamond" class="disabled d3d-btn view-vr-btn">💎</div>
+	<div id="give-heart" class="disabled d3d-btn view-vr-btn">❤</div>
+</div>
+	
